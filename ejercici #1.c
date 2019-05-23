ca229b868fd8cacbe463c6ab804f30e16a91e4e0
Robert <20180840@ce.pucmm.edu.do>
2019-05-23 01:49:40 -0400

    cambios 1.0

---
+++ ejercici #1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tomarmatriz(int[filas][columnas],int filas,int columnas);
+void mostrarmatriz(int[],int,int);
+void invmatriz(int[],int[],int,int);
+
+
+int main(){
+
+   const int i,j;
+   int matriz[i][j],matrizinv[j][i];
+
+   printf("Introduzca las dimensiones de la matriz: ");
+   scanf("%d %d",&i,&j);
+
+   tomarmatriz(matriz,i,j);
+
+   system("cls");
+
+   printf("Matriz: \n\n");
+
+   mostrarmatriz(matriz,i,j);
+
+   invmatriz(matriz,matrizinv,i,j);
+
+   printf("Matriz inversa: \n\n");
+
+   mostrarmatriz(matrizinv,j,i);
+
+
+
+
+   return 0;
+}
+
+void tomarmatriz(int matriz[][],int filas,int columnas){
+
+   int i,j;
+
+   for(i=0; i<filas; i++){
+      for(j=0; j<columnas; j++){
+         printf("Inserte el valor de la posici%cn (%d,%d): ",162,i,j);
+         scanf("%d",&matriz[i][j]);
+         printf("\n");
+      }
+   }
+
+   return;
+}
